e53dd638a4e10aa1fdca8ccb331a4814d77c3a4a
aburrell <ab763@le.ac.uk>
2018-03-15 13:17:51 -0500

    documentation compilation
    Hopefully fixed documentation compilation by including requirement for
    sphinx_rtd_theme and updating coverage source directory to the new
    directory structure.

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,8 @@ version = release = u'2.0.1'
 # on_rtd is whether we are on readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
+# only import and set the theme if we're building docs locally
+if not on_rtd:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
