1c5b01ef7a0866ee8ffc1dbe96fe0ec421b9cb06
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2018-08-01 17:11:18 -0400

    includes for C code v2.5
    Updated include files for C code in v2.5.  Kept definitions needed for windows compilation.

--- c_aacgmv2/include/aacgmlib_v2.h
+++ c_aacgmv2/include/aacgmlib_v2.h
@@ -1,5 +1,4 @@
 #ifndef _AACGMLIB_
-
 #define _AACGMLIB_
 
 /*****************************************************************************
@@ -21,6 +20,7 @@
 #define ALLOWTRACE 4  /* if height is >2000 km use tracing, else use coefs */
 #define BADIDEA    8  /* use coefficients above 2000 km; Terrible idea!!   */
 #define GEOCENTRIC 16 /* assume inputs are geocentric with sphere RE       */
+#define VERBOSE    32 /* set verbosity for output                          */
 #ifndef M_PI
   #define M_PI 3.14159265358979323846 /* define M_PI if not already */
 #endif
@@ -44,28 +44,26 @@ void AACGM_v2_Alt2CGM(double r_height_in, double r_lat_alt, double *r_lat_adj);
 int AACGM_v2_CGM2Alt(double r_height_in, double r_lat_in, double *r_lat_adj);
 double AACGM_v2_Sgn(double a, double b);
 int convert_geo_coord_v2(double lat_in, double lon_in, double height_in,
-                         double *lat_out, double *lon_out, int flag, int order,
-			 char *igrf_filename);
+                         double *lat_out, double *lon_out, int flag, int order);
 int AACGM_v2_LoadCoefFP(FILE *fp, int code);
 int AACGM_v2_LoadCoef(char *fname, int code);
-int AACGM_v2_LoadCoefs(int year, char *root);
-int AACGM_v2_TimeInterp(char *root);
+int AACGM_v2_LoadCoefs(int year);
+int AACGM_v2_TimeInterp(void);
 void AACGM_v2_errmsg(int ecode);
 int AACGM_v2_Trace(double lat_in, double lon_in, double alt,
-                   double *lat_out, double *lon_out, char *igrf_filename);
+                   double *lat_out, double *lon_out);
 int AACGM_v2_Trace_inv(double lat_in, double lon_in, double alt,
-                       double *lat_out, double *lon_out, char *igrf_filename);
+                       double *lat_out, double *lon_out);
 
 
 /* public functions */
 int AACGM_v2_Convert(double in_lat, double in_lon, double height,
-                     double *out_lat, double *out_lon, double *r, int code,
-		     char *igrf_filename);
+                     double *out_lat, double *out_lon, double *r, int code);
 int AACGM_v2_SetDateTime(int year, int month, int day,
-                         int hour, int minute, int second, char *root);
+                         int hour, int minute, int second);
 int AACGM_v2_GetDateTime(int *year, int *month, int *day,
                          int *hour, int *minute, int *second, int *dayno);
-int AACGM_v2_SetNow(char *root);
+int AACGM_v2_SetNow(void);
 
 #endif
 
