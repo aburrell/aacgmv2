1b800f6f990aba58a411a0c7c541890b99734d1d
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2018-08-06 09:12:16 -0400

    import of os.environ
    Forgot to import os.environ.  Fixed this bug.

--- aacgmv2/__init__.py
+++ aacgmv2/__init__.py
@@ -41,6 +41,7 @@ _aacgmv2.inv_mlt_convert_yrsec
 """
 from __future__ import print_function
 import os.path as _path
+import os.environ as _environ
 import logbook as logging
 
 __version__ = "2.5.0"
@@ -52,13 +53,13 @@ IGRF_COEFFS = _path.join(_path.realpath(_path.dirname(__file__)),
                          'magmodel_1590-2015.txt')
 
 # If not defined, set the IGRF and AACGM environment variables
-if not 'IGRF_COEFFS' in os.environ.keys():
-    os.environ['IGRF_COEFFS'] = IGRF_COEFFS
+if not 'IGRF_COEFFS' in _environ.keys():
+    _environ['IGRF_COEFFS'] = IGRF_COEFFS
 else:
     print("using local IGRF coefficients instead of AACGMV2 defaults")
     
-if not 'AACGM_V2_DAT_PREFIX' in os.environ.keys():
-    os.environ['AACGM_V2_DAT_PREFIX'] = AACGM_V2_DAT_PREFIX
+if not 'AACGM_V2_DAT_PREFIX' in _environ.keys():
+    _environ['AACGM_V2_DAT_PREFIX'] = AACGM_V2_DAT_PREFIX
 else:
     print("using local AACGM coefficients instead of AACGMV2 defaults")
 
