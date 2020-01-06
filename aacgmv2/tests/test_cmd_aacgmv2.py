e925d13c77985f5afcdd1ce8cdda47487530e4c2
Angeline G. Burrell <angeline.burrell@nrl.navy.mil>
2020-01-06 13:38:14 -0500

    STY: removed unused import

--- aacgmv2/tests/test_cmd_aacgmv2.py
+++ aacgmv2/tests/test_cmd_aacgmv2.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import division, absolute_import, unicode_literals, print_function
+from __future__ import division, absolute_import, unicode_literals
 
 import subprocess
 import numpy as np
