e20d68897976c256d048a45d44a3c638e0c9e2bb
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2020-08-27 09:17:54 -0400

    STY: flake8 and python_versions
    Made PEP8 changes and added python_versions as output.

--- ci/bootstrap.py
+++ ci/bootstrap.py
@@ -3,27 +3,33 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 import os
+import subprocess
 import sys
+
 
 if __name__ == "__main__":
     base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
     print("Project path: {0}".format(base_path))
     env_path = os.path.join(base_path, ".tox", "bootstrap")
+
     if sys.platform == "win32":
         bin_path = os.path.join(env_path, "Scripts")
     else:
         bin_path = os.path.join(env_path, "bin")
+
     if not os.path.exists(env_path):
-        import subprocess
         print("Making bootstrap env in: {0} ...".format(env_path))
         try:
             subprocess.check_call(["virtualenv", env_path])
         except Exception:
             subprocess.check_call([sys.executable, "-m", "virtualenv",
                                    env_path])
+
         print("Installing `jinja2` and `matrix` into bootstrap environment ...")
         subprocess.check_call([os.path.join(bin_path, "pip"), "install",
                                "jinja2", "matrix"])
+
     activate = os.path.join(bin_path, "activate_this.py")
     exec(compile(open(activate, "rb").read(), activate, "exec"),
          dict(__file__=activate))
@@ -34,15 +40,11 @@ if __name__ == "__main__":
     jinja = jinja2.Environment(
         loader=jinja2.FileSystemLoader(os.path.join(base_path, "ci",
                                                     "templates")),
-        trim_blocks=True,
-        lstrip_blocks=True,
-        keep_trailing_newline=True
-    )
+        trim_blocks=True, lstrip_blocks=True, keep_trailing_newline=True)
     tox_envs = {}
     for (alias, conf) in matrix.from_file(os.path.join(base_path,
                                                        "setup.cfg")).items():
         python = conf["python_versions"]
-        #deps = conf["dependencies"]
         if "coverage_flags" in conf:
             cover = {"false": False,
                      "true": True}[conf["coverage_flags"].lower()]
@@ -51,7 +53,6 @@ if __name__ == "__main__":
 
         tox_envs[alias] = {
             "python": "python" + python if "py" not in python else python,
-            #"deps": deps.split(),
         }
         if "coverage_flags" in conf:
             tox_envs[alias].update(cover=cover)
@@ -60,6 +61,8 @@ if __name__ == "__main__":
 
     for name in os.listdir(os.path.join("ci", "templates")):
         with open(os.path.join(base_path, name), "w") as fh:
-            fh.write(jinja.get_template(name).render(tox_environments=tox_envs))
+            fh.write(jinja.get_template(name).render(
+                tox_environments=tox_envs,
+                python_versions=conf["python_versions"]))
         print("Wrote {}".format(name))
     print("DONE.")
