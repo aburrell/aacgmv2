4a8ad210aafe35a7bd8ad2a79e690d644bb58c33
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2020-09-03 07:14:44 -0400

    TST: utils unit tests
    Added utils unit tests to cover uncovered lines by adding high and low years.

--- aacgmv2/tests/test_utils_aacgmv2.py
+++ aacgmv2/tests/test_utils_aacgmv2.py
@@ -26,12 +26,24 @@ class TestUtilsAACGMV2:
 
         np.testing.assert_allclose(self.out, [-179.2004, -23.0431], rtol=1.0e-4)
 
-    def test_igrf_dipole_axis(self):
+    @pytest.mark.parametrize('year', [(1500), (2110)])
+    def test_subsol_raises_time_range(self, year):
+        """Test the routine failure for out-of-range dates"""
+        with pytest.raises(ValueError, match="subsol valid between 1601-2100"):
+            utils.subsol(year, int(self.dtime.strftime("%j")),
+                         self.dtime.hour * 3600.0
+                         + self.dtime.minute * 60.0 + self.dtime.second)
+
+    @pytest.mark.parametrize('year,ref',
+                             [(1500, [0.141408, -0.48357, 0.86381]),
+                              (2015, [0.050281, -0.16057, 0.98574]),
+                              (2110, [0.027069, -0.08006, 0.99642])])
+    def test_igrf_dipole_axis(self, year, ref):
         """Test the IGRF dipole axis calculation"""
+        self.dtime = dt.datetime(year, 1, 1)
         self.out = utils.igrf_dipole_axis(self.dtime)
 
-        np.testing.assert_allclose(self.out, [0.050281, -0.16057, 0.98574],
-                                   rtol=1.0e-4)
+        np.testing.assert_allclose(self.out, ref, rtol=1.0e-4)
 
     @pytest.mark.parametrize('gc_lat,gd_lat,mult',
                              [(45.0, 45.1924, False),
